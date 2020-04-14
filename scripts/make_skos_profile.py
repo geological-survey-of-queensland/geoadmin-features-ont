2610525ec87560962143323b1e49c575a1fb4c1c
Nicholas Car <nicholas.car@csiro.au>
2020-04-14 15:24:38 +1000

    tidy-up from David comments

--- scripts/make_skos_profile.py
+++ scripts/make_skos_profile.py
@@ -7,10 +7,9 @@ g = rdflib.Graph().parse("../geoadminfeatures.ttl", format="turtle")
 g_skos = rdflib.Graph()
 g_skos.bind('skos', rdflib.namespace.SKOS)
 g_skos.bind('dcterms', rdflib.namespace.DCTERMS)
-g_skos.bind('prof', rdflib.Namespace("http://www.w3.org/ns/dx/prof/"))
 g_skos.bind('owl', rdflib.Namespace("http://www.w3.org/2002/07/owl#"))
 g_skos.bind('sdo', rdflib.Namespace("https://schema.org/"))
-g_skos.bind('geof', rdflib.Namespace("http://linked.data.gov.au/def/geofeatures/"))
+g_skos.bind('geoad', rdflib.Namespace("http://linked.data.gov.au/def/geoadminfeatures#"))
 
 # get Ontology as a ConceptScheme
 q = """
@@ -47,29 +46,29 @@ g_skos += g.query(q)
 
 # rename the vocab to add "Types"
 g_skos.remove((
-    rdflib.URIRef("http://linked.data.gov.au/def/geofeatures"),
+    rdflib.URIRef("http://linked.data.gov.au/def/geoadminfeatures"),
     rdflib.namespace.SKOS.prefLabel,
     None
 ))
 g_skos.add((
-    rdflib.URIRef("http://linked.data.gov.au/def/geofeatures"),
+    rdflib.URIRef("http://linked.data.gov.au/def/geoadminfeatures"),
     rdflib.namespace.SKOS.prefLabel,
-    rdflib.Literal("Geologic Feature Types", lang='en')
+    rdflib.Literal("GeoAdmin Feature Types", lang='en')
 ))
 
 # replace the ont definition with this custom one for the vocab
 g_skos.remove((
-    rdflib.URIRef("http://linked.data.gov.au/def/geofeatures"),
+    rdflib.URIRef("http://linked.data.gov.au/def/geoadminfeatures"),
     rdflib.namespace.SKOS.definition,
     None
 ))
 g_skos.add((
-    rdflib.URIRef("http://linked.data.gov.au/def/geofeatures"),
+    rdflib.URIRef("http://linked.data.gov.au/def/geoadminfeatures"),
     rdflib.namespace.SKOS.definition,
     rdflib.Literal(
-        "This vocabulary lists types of geological features relevant to the duties of the Geological Survey of "
-        "Queensland. It is automatically derived from GSQ's Geologic Features Ontology "
-        "(http://linked.data.gov.au/def/geofeatures)", lang='en')
+        "This vocabulary lists types of geological administrative features relevant to the duties of the Geological "
+        "Survey of Queensland. It is automatically derived from GSQ's Geological Administrative Features Ontology "
+        "(http://linked.data.gov.au/def/geoadminfeatures)", lang='en')
 ))
 
 # get Classes as Concepts
