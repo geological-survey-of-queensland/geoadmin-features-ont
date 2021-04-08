c882dacecf988c20e9df43d7bde653b2240c01f4
Nicholas Car <nicholas.car@surroundaustralia.com>
2021-04-08 10:38:13 +1000

    http -> https for linked.data.gov.au

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 <img src="images/gsq.jpg" style="width:25%" />  
 
 # GSQ Geological Administrative Features Ontology
-This ontology defines a series of subclasses of the [GeoSPARQL Ontology's](https://www.opengeospatial.org/standards/geosparql) `Feature` class that are used by the [Geological Survey of Queensland](http://linked.data.gov.au/org/gsq) to admininster geologically-based resource extraction in the Australian state of Queensland. The classes include `Block`, `Permit` etc., some of which are defined in Queensland or Australian legislation.
+This ontology defines a series of subclasses of the [GeoSPARQL Ontology's](https://www.opengeospatial.org/standards/geosparql) `Feature` class that are used by the [Geological Survey of Queensland](https://linked.data.gov.au/org/gsq) to admininster geologically-based resource extraction in the Australian state of Queensland. The classes include `Block`, `Permit` etc., some of which are defined in Queensland or Australian legislation.
 
 
 ## License
@@ -11,7 +11,7 @@ The content of this API is licensed for use under the [Creative Commons 4.0 Lice
 ## Citation
 If you wish to cite this ontology, please do so like this:
 
-Geological Survey of Queensland (2019) "Geological Administrative Features Ontology". An OWL ontology defining classes of geospatial Feature relevant to the mineral adminstration duties of the Geological Survey of Queensland. http://linked.data.gov.au/def/geoadminfeatures
+Geological Survey of Queensland (2019) "Geological Administrative Features Ontology". An OWL ontology defining classes of geospatial Feature relevant to the mineral adminstration duties of the Geological Survey of Queensland. https://linked.data.gov.au/def/geoadminfeatures
 
 
 ## Contacts
--- scripts/make_skos_profile.py
+++ scripts/make_skos_profile.py
@@ -9,7 +9,7 @@ g_skos.bind('skos', rdflib.namespace.SKOS)
 g_skos.bind('dcterms', rdflib.namespace.DCTERMS)
 g_skos.bind('owl', rdflib.Namespace("http://www.w3.org/2002/07/owl#"))
 g_skos.bind('sdo', rdflib.Namespace("https://schema.org/"))
-g_skos.bind('geoad', rdflib.Namespace("http://linked.data.gov.au/def/geoadminfeatures#"))
+g_skos.bind('geoad', rdflib.Namespace("https://linked.data.gov.au/def/geoadminfeatures#"))
 
 # get Ontology as a ConceptScheme
 q = """
@@ -46,29 +46,29 @@ g_skos += g.query(q)
 
 # rename the vocab to add "Types"
 g_skos.remove((
-    rdflib.URIRef("http://linked.data.gov.au/def/geoadminfeatures"),
+    rdflib.URIRef("https://linked.data.gov.au/def/geoadminfeatures"),
     rdflib.namespace.SKOS.prefLabel,
     None
 ))
 g_skos.add((
-    rdflib.URIRef("http://linked.data.gov.au/def/geoadminfeatures"),
+    rdflib.URIRef("https://linked.data.gov.au/def/geoadminfeatures"),
     rdflib.namespace.SKOS.prefLabel,
     rdflib.Literal("GeoAdmin Feature Types", lang='en')
 ))
 
 # replace the ont definition with this custom one for the vocab
 g_skos.remove((
-    rdflib.URIRef("http://linked.data.gov.au/def/geoadminfeatures"),
+    rdflib.URIRef("https://linked.data.gov.au/def/geoadminfeatures"),
     rdflib.namespace.SKOS.definition,
     None
 ))
 g_skos.add((
-    rdflib.URIRef("http://linked.data.gov.au/def/geoadminfeatures"),
+    rdflib.URIRef("https://linked.data.gov.au/def/geoadminfeatures"),
     rdflib.namespace.SKOS.definition,
     rdflib.Literal(
         "This vocabulary lists types of geological administrative features relevant to the duties of the Geological "
         "Survey of Queensland. It is automatically derived from GSQ's Geological Administrative Features Ontology "
-        "(http://linked.data.gov.au/def/geoadminfeatures)", lang='en')
+        "(https://linked.data.gov.au/def/geoadminfeatures)", lang='en')
 ))
 
 # get Classes as Concepts
@@ -140,9 +140,9 @@ g_skos += g.query(q)
 # add in a hasTopConcept for Admininstrative Feature since the generic hastopConcept adder doesn't pick it up
 # since it has it's own subClassOf link (to geo:Feature)
 g_skos.add((
-    rdflib.URIRef("http://linked.data.gov.au/def/geoadminfeatures"),
+    rdflib.URIRef("https://linked.data.gov.au/def/geoadminfeatures"),
     rdflib.namespace.SKOS.hasTopConcept,
-    rdflib.URIRef("http://linked.data.gov.au/def/geoadminfeatures#AdministrativeFeature")
+    rdflib.URIRef("https://linked.data.gov.au/def/geoadminfeatures#AdministrativeFeature")
 ))
 
 with open("../geoadmin-feature-types.ttl", "w") as f:
